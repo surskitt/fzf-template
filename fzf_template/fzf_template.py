76abbda4973f081f0b8e66e25306e86154b05dbc
sharktamer <donohoe.shane@gmail.com>
2019-05-26 20:19:49 +0100

    functions and tests for pulling values from conf, for applying values to template

--- fzf_template/fzf_template.py
+++ fzf_template/fzf_template.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+
+def conf_keys(conf_dict):
+    return conf_dict['values'].keys()
+
+
+def conf_get_value_dict(conf_dict, k):
+    return conf_dict['values'][k]
+
+
+def apply_template(template, values):
+    return template.format(template, **values)
--- tests/test_fzf_template.py
+++ tests/test_fzf_template.py
@@ -10,9 +10,51 @@ from fzf_template import fzf_template
 
 
 @pytest.fixture
-def conf_file():
-    pass
+def conf_dict():
+    conf_dict = {
+        'values': {
+            'key1': {
+                'a': 'test1',
+                'b': 'test2'
+            },
+            'key2': {
+                'a': 'test3',
+                'b': 'test4'
+            }
+        },
+        'templates': [
+            {
+                'src': 'test.conf.template',
+                'dest': 'test.conf'
+            },
+            {
+                'src': 'test2.conf.template',
+                'dest': 'test2.conf'
+            }
+        ]
+    }
+
+    return conf_dict
 
 
-def test_load_template():
-    pass
+def test_conf_keys(conf_dict):
+    expected = ['key1', 'key2']
+    keys = fzf_template.conf_keys(conf_dict)
+
+    assert list(keys) == expected
+
+
+def test_conf_get_value_dict(conf_dict):
+    expected = {'a': 'test1', 'b': 'test2'}
+    values = fzf_template.conf_get_value_dict(conf_dict, 'key1')
+
+    assert values == expected
+
+
+def test_apply_template():
+    expected = 'value1: test1 test2'
+    template = 'value1: {a} {b}'
+    values = {'a': 'test1', 'b': 'test2'}
+    templated = fzf_template.apply_template(template, values)
+
+    assert templated == expected
